ce16ef6d181626d63611a86a6ba6df17d8adc760
fabiomelo <fabio.fam2@gmail.com>
2016-12-12 22:06:02 -0300

    fixes

---
+++ middlewares/validate.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var messages = require('../utils/messages');
+var httpStatusCode = require('../utils/httpStatusCode');
+
+
+module.exports = {
+  updatePosition: function(request, response, next){
+      next();
+  }
+};
