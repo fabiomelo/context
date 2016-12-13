778864cb488731a3753068ea491772a717779baa
fabiomelo <fabio.fam2@gmail.com>
2016-12-12 22:05:03 -0300

    fixes

--- convoy/routes/routes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-
-
-module.exports = function(app) {
-  	var controller = app.controllers.convoy;
-  	app.post('/register', controller.register);
-  	app.post('/updatePosition', controller.updatePosition);
-};
